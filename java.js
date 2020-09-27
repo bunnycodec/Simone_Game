7c2417bc250e9992ae9194c5c4678e530e236b6c
Sunny Kumar <sunnykumar@bunnycodec.com>
2020-09-27 16:10:41 +0530

    UI Updated and Bootstrap 5 was introduced

--- java.js
+++ java.js
@@ -2,25 +2,20 @@ var random = [];
 var litID = [];
 var clicked = [];
 var i;
-var j=0;
+var j = 0;
 var count = 1;
 var on;
 var off;
-var strict;
 var x;
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('#count').hide();
-    $('#on').click(function() {
+    $('#on').click(function () {
         $('#count').show();
-        for (i=0; i<20; i++) {
+        for (i = 0; i < 20; i++) {
             random[i] = Math.ceil((Math.random() * 4));
         }
-
-        $('#strict').click(function () {
-            strict=1;
-        });
 
         $('#start').click(function () {
             $('#count').html(count);
@@ -36,7 +31,7 @@ $(document).ready(function(){
             }, 250);
             checking();
         });
-        
+
         $('#two').click(function () {
             $('#two').addClass('light2');
             $('#audio2')[0].play();
@@ -46,7 +41,7 @@ $(document).ready(function(){
             }, 250);
             checking();
         });
-        
+
         $('#three').click(function () {
             $('#three').addClass('light3');
             $('#audio3')[0].play();
@@ -56,7 +51,7 @@ $(document).ready(function(){
             }, 250);
             checking();
         });
-        
+
         $('#four').click(function () {
             $('#four').addClass('light4');
             $('#audio4')[0].play();
@@ -69,7 +64,7 @@ $(document).ready(function(){
 
     });
 
-    $('#reset').click(function() {
+    $('#reset').click(function () {
         location.reload();
     });
 
@@ -78,16 +73,14 @@ $(document).ready(function(){
         if (count <= 10) {
             off = 400;
             on = 800;
-        }
-        else {
+        } else {
             off = 250;
             on = 500;
         }
 
         x = setInterval(function () {
 
-            if (random[j] == 1) 
-            {
+            if (random[j] == 1) {
                 $('#one').addClass('light1');
                 $('#audio1')[0].play();
                 litID.push(1);
@@ -95,10 +88,7 @@ $(document).ready(function(){
                 setTimeout(function () {
                     $('#one').removeClass('light1');
                 }, off);
-            }
-
-            else if (random[j] == 2) 
-            {
+            } else if (random[j] == 2) {
                 $('#two').addClass('light2');
                 $('#audio2')[0].play();
                 litID.push(2);
@@ -107,10 +97,7 @@ $(document).ready(function(){
                     $('#two').removeClass('light2');
                 }, off);
 
-            }
-
-            else if (random[j] == 3) 
-            {
+            } else if (random[j] == 3) {
                 $('#three').addClass('light3');
                 $('#audio3')[0].play();
                 litID.push(3);
@@ -118,9 +105,7 @@ $(document).ready(function(){
                 setTimeout(function () {
                     $('#three').removeClass('light3');
                 }, off);
-            }
-
-            else {
+            } else {
                 $('#four').addClass('light4');
                 $('#audio4')[0].play();
                 litID.push(4);
@@ -132,61 +117,38 @@ $(document).ready(function(){
 
             j++;
 
-            if (j>=count) {
+            if (j >= count) {
                 clearInterval(x);
             }
 
         }, 500);
     }
-    
+
     function checking() {
-    if (litID.length === clicked.length) 
-    {
-        if (litID.join() === clicked.join()) 
-        {
-            if (count === 20) 
-            {
-                setTimeout(function () {
-                    alert("You win!");
-                    location.reload();
-                }, 1000);
-            }
-
-            else 
-            {
-                setTimeout(function () {
-                    $('#count').html(count + 1);
-                    count++;
-                    litID = [];
-                    clicked = [];
-                    j = 0;
-                    change1();
-                }, 1000);
-            }
-        }
-
-        else 
-        {
-            if (strict == 1) 
-            {
-                location.reload();
-
-            }
-
-            else {
-                setTimeout(function () {
-                    $('#count').text('!!');
-
-                    litID = [];
-                    clicked = [];
-                    j = 0;
-                    change1();
-                }, 1000);
+        if (litID.length === clicked.length) {
+            if (litID.join() === clicked.join()) {
+                if (count === 20) {
+                    setTimeout(function () {
+                        alert("You win!");
+                        location.reload();
+                    }, 1000);
+                } else {
+                    setTimeout(function () {
+                        $('#count').html(count + 1);
+                        count++;
+                        litID = [];
+                        clicked = [];
+                        j = 0;
+                        change1();
+                    }, 1000);
+                }
+            } else {
+                $('#count').text('That\'s Wrong !! Your Score : ' + count);
+                $('#start').hide();
+                $('#on').hide();
             }
         }
     }
-}
 
 
 });
-
